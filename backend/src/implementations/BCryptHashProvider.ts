e8ca25070d37519739b697c7bfbdf1db1b0f12d5
Pedro <pedro-gomes17@hotmail.com>
2020-10-19 20:07:00 -0300

    Functions for generate and compare password hash

---
+++ backend/src/implementations/BCryptHashProvider.ts
@@ -0,0 +1,14 @@
+import { compare, hash } from 'bcryptjs';
+
+class BCryptHashProvider {
+
+  public async generateHash(payload: string): Promise<string> {
+    return hash(payload, 8);
+  }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    return compare(payload, hashed);
+  } 
+}
+
+export default new BCryptHashProvider();
