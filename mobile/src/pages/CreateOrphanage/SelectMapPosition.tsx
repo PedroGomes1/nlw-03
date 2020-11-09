4c8f15242132698a0528a85094deecd7e2490300
Pedro <pedro-gomes17@hotmail.com>
2020-11-09 17:28:31 -0300

    Implementation of the initial user instruction click on the map

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -1,52 +1,113 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Dimensions, Text } from 'react-native';
-
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useRoute } from '@react-navigation/native';
 import MapView, { Marker, MapEvent } from 'react-native-maps';
-
+import hand from '../../images/hand.png';
 import mapMarkerImg from '../../images/map-marker.png';
 
+interface SelectedMapRouteParams {
+  currentLocation: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
 const SelectMapPosition: React.FC = () => {
+  const route = useRoute();
+  const params = route.params as SelectedMapRouteParams;
   const navigation = useNavigation();
 
-  const [position, setPosition] = useState({
+  const [currentLocation] = useState({
+    latitude: params.currentLocation.latitude,
+    longitude: params.currentLocation.longitude,
+  });
+
+  const [positionSelected, setPositionSelected] = useState({
     latitude: 0,
     longitude: 0,
   });
 
   function handleNextStep(): void {
-    navigation.navigate('OrphanageData', { position });
+    navigation.navigate('OrphanageData', { position: positionSelected });
   }
 
   function handleSelectMapPosition(event: MapEvent): void {
-    setPosition(event.nativeEvent.coordinate); // Salva a latitude e longitude
+    setPositionSelected(event.nativeEvent.coordinate); // Salva a latitude e longitude
   }
 
   return (
     <View style={styles.container}>
-      <MapView
-        initialRegion={{
-          latitude: -22.0515076,
-          longitude: -46.9710888,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-        onPress={handleSelectMapPosition}
-        style={styles.mapStyle}
-      >
-        {position.latitude !== 0 && (
-          <Marker
-            icon={mapMarkerImg}
-            coordinate={{
-              latitude: position.latitude,
-              longitude: position.longitude,
+      {positionSelected.latitude !== 0 ? (
+        <>
+          <MapView
+            initialRegion={{
+              latitude: currentLocation.latitude,
+              longitude: currentLocation.longitude,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
             }}
-          />
-        )}
-      </MapView>
+            onPress={handleSelectMapPosition}
+            style={styles.mapStyle}
+          >
+            <Marker
+              // icon={handleImg}
+              coordinate={{
+                latitude: positionSelected.latitude,
+                longitude: positionSelected.longitude,
+              }}
+            />
+          </MapView>
+        </>
+      ) : (
+        <>
+          <View style={styles.containerWithoutSelecting}>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.selectButton}
+              onPress={() =>
+                setPositionSelected({
+                  latitude: currentLocation.latitude,
+                  longitude: currentLocation.longitude,
+                })}
+            >
+              <Image source={hand} />
+              <Text style={styles.textInstruction}>
+                Toque no mapa para adicionar um orfanato
+              </Text>
+            </TouchableOpacity>
 
-      {position.latitude !== 0 && (
+            <MapView
+              initialRegion={{
+                latitude: currentLocation.latitude,
+                longitude: currentLocation.longitude,
+                latitudeDelta: 0.008,
+                longitudeDelta: 0.008,
+              }}
+              onPress={handleSelectMapPosition}
+              style={styles.mapStyle}
+            >
+              <Marker
+                icon={mapMarkerImg}
+                coordinate={{
+                  latitude: positionSelected.latitude,
+                  longitude: positionSelected.longitude,
+                }}
+              />
+            </MapView>
+          </View>
+        </>
+      )}
+
+      {positionSelected.latitude !== 0 && (
         <RectButton style={styles.nextButton} onPress={handleNextStep}>
           <Text style={styles.nextButtonText}>Próximo</Text>
         </RectButton>
@@ -61,9 +122,45 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
 
+  containerWithoutSelecting: {
+    position: 'relative',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  selectButton: {
+    position: 'relative',
+    top: '55%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    opacity: 0.8,
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+    backgroundColor: '#15B6D6',
+    zIndex: 99,
+  },
+
   mapStyle: {
+    position: 'relative',
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+
+  image: {
+    position: 'relative',
+    borderWidth: 1,
+    borderColor: '#004',
+  },
+
+  textInstruction: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 24,
+    lineHeight: 34,
+    color: '#FFFFFF',
+    maxWidth: 203,
+    textAlign: 'center',
+    marginTop: 24,
   },
 
   nextButton: {
